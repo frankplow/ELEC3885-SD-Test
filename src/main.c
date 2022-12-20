a98ea2cc00cb614e98a1484acb17ad7a530fb787
Frank Plowman <post@frankplowman.com>
2022-12-20 09:56:44 +0000

    clang-format

--- src/main.c
+++ src/main.c
@@ -1,202 +1,106 @@
-/**
-  ******************************************************************************
-  * @file    GPIO/GPIO_IOToggle/Src/main.c
-  * @author  MCD Application Team
-  * @brief   This example describes how to configure and use GPIOs through
-  *          the STM32F4xx HAL API.
-  ******************************************************************************
-  * @attention
-  *
-  * Copyright (c) 2017 STMicroelectronics.
-  * All rights reserved.
-  *
-  * This software is licensed under terms that can be found in the LICENSE file
-  * in the root directory of this software component.
-  * If no LICENSE file comes with this software, it is provided AS-IS.
-  *
-  ******************************************************************************
-  */
-
-/* Includes ------------------------------------------------------------------*/
 #include "stm32f4xx_hal.h"
 #include "stm32f4xx_nucleo_144.h"
-/** @addtogroup STM32F4xx_HAL_Examples
-  * @{
-  */
 
-/** @addtogroup GPIO_IOToggle
-  * @{
-  */
+// global variables
+static GPIO_InitTypeDef gpio_cfg;
 
-/* Private typedef -----------------------------------------------------------*/
-/* Private define ------------------------------------------------------------*/
-/* Private macro -------------------------------------------------------------*/
-/* Private variables ---------------------------------------------------------*/
-static GPIO_InitTypeDef  GPIO_InitStruct;
+// function prototypes
+static void sysclk_cfg(void);
+static void error_handler(void);
 
-/* Private function prototypes -----------------------------------------------*/
-static void SystemClock_Config(void);
-static void Error_Handler(void);
-
-/* Private functions ---------------------------------------------------------*/
-
-/**
-  * @brief  Main program
-  * @param  None
-  * @retval None
-  */
-int main(void)
-{
-  /* This sample code shows how to use GPIO HAL API to toggle LED1 and LED2 IOs
-    in an infinite loop. */
-
-  /* STM32F4xx HAL library initialization:
-       - Configure the Flash prefetch
-       - Systick timer is configured by default as source of time base, but user 
-         can eventually implement his proper time base source (a general purpose 
-         timer for example or other time source), keeping in mind that Time base 
-         duration should be kept 1ms since PPP_TIMEOUT_VALUEs are defined and 
-         handled in milliseconds basis.
-       - Set NVIC Group Priority to 4
-       - Low Level Initialization
-     */
+int main(void) {
+  // initialise systems
   HAL_Init();
+  sysclk_cfg();
 
-  /* Configure the system clock to 180 MHz */
-  SystemClock_Config();
-  
-  /* -1- Enable GPIO Clock (to be able to program the configuration registers) */
+  // configure LEDs
   LED1_GPIO_CLK_ENABLE();
   LED2_GPIO_CLK_ENABLE();
+  LED3_GPIO_CLK_ENABLE();
+  gpio_cfg.Mode = GPIO_MODE_OUTPUT_PP;
+  gpio_cfg.Pull = GPIO_PULLUP;
+  gpio_cfg.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
+  gpio_cfg.Pin = LED1_PIN;
+  HAL_GPIO_Init(LED1_GPIO_PORT, &gpio_cfg);
+  gpio_cfg.Pin = LED2_PIN;
+  HAL_GPIO_Init(LED2_GPIO_PORT, &gpio_cfg);
+  gpio_cfg.Pin = LED3_PIN;
+  HAL_GPIO_Init(LED3_GPIO_PORT, &gpio_cfg);
 
-  /* -2- Configure IO in output push-pull mode to drive external LEDs */
-  GPIO_InitStruct.Mode  = GPIO_MODE_OUTPUT_PP;
-  GPIO_InitStruct.Pull  = GPIO_PULLUP;
-  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
-
-  GPIO_InitStruct.Pin = LED1_PIN;
-  HAL_GPIO_Init(LED1_GPIO_PORT, &GPIO_InitStruct);
-  GPIO_InitStruct.Pin = LED2_PIN;
-  HAL_GPIO_Init(LED2_GPIO_PORT, &GPIO_InitStruct);
-
-  /* -3- Toggle IO in an infinite loop */
-  while (1)
-  {
+  while (1) {
     HAL_GPIO_TogglePin(LED1_GPIO_PORT, LED1_PIN);
-    /* Insert delay 100 ms */
     HAL_Delay(100);
     HAL_GPIO_TogglePin(LED2_GPIO_PORT, LED2_PIN);
-    /* Insert delay 100 ms */
+    HAL_Delay(100);
+    HAL_GPIO_TogglePin(LED3_GPIO_PORT, LED3_PIN);
     HAL_Delay(100);
   }
 }
 
 /**
-  * @brief  System Clock Configuration
-  *         The system Clock is configured as follow : 
-  *            System Clock source            = PLL (HSE)
-  *            SYSCLK(Hz)                     = 180000000
-  *            HCLK(Hz)                       = 180000000
-  *            AHB Prescaler                  = 1
-  *            APB1 Prescaler                 = 4
-  *            APB2 Prescaler                 = 2
-  *            HSE Frequency(Hz)              = 8000000
-  *            PLL_M                          = 8
-  *            PLL_N                          = 360
-  *            PLL_P                          = 2
-  *            PLL_Q                          = 7
-  *            VDD(V)                         = 3.3
-  *            Main regulator output voltage  = Scale1 mode
-  *            Flash Latency(WS)              = 5
-  * @param  None
-  * @retval None
-  */
-static void SystemClock_Config(void)
-{
-  RCC_ClkInitTypeDef RCC_ClkInitStruct;
-  RCC_OscInitTypeDef RCC_OscInitStruct;
+ * @brief  System Clock Configuration
+ *         The system Clock is configured as follow :
+ *            System Clock source            = PLL (HSE)
+ *            SYSCLK(Hz)                     = 180000000
+ *            HCLK(Hz)                       = 180000000
+ *            AHB Prescaler                  = 1
+ *            APB1 Prescaler                 = 4
+ *            APB2 Prescaler                 = 2
+ *            HSE Frequency(Hz)              = 8000000
+ *            PLL_M                          = 8
+ *            PLL_N                          = 360
+ *            PLL_P                          = 2
+ *            PLL_Q                          = 7
+ *            VDD(V)                         = 3.3
+ *            Main regulator output voltage  = Scale1 mode
+ *            Flash Latency(WS)              = 5
+ * @param  None
+ * @retval None
+ */
+static void sysclk_cfg(void) {
+  RCC_OscInitTypeDef rccosc_cfg;
+  RCC_ClkInitTypeDef rccclk_cfg;
 
-  /* Enable Power Control clock */
   __HAL_RCC_PWR_CLK_ENABLE();
-  
-  /* The voltage scaling allows optimizing the power consumption when the device is 
-     clocked below the maximum system frequency, to update the voltage scaling value 
-     regarding system frequency refer to product datasheet.  */
   __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
-  
-  /* Enable HSE Oscillator and activate PLL with HSE as source */
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
-  RCC_OscInitStruct.HSEState = RCC_HSE_BYPASS;
-  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
-  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
-  RCC_OscInitStruct.PLL.PLLM = 8;
-  RCC_OscInitStruct.PLL.PLLN = 360;
-  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
-  RCC_OscInitStruct.PLL.PLLQ = 7;
-  if(HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
-  {
-    /* Initialization Error */
-    Error_Handler();
+
+  // configure oscillator
+  rccosc_cfg.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+  rccosc_cfg.HSEState = RCC_HSE_BYPASS;
+  rccosc_cfg.PLL.PLLState = RCC_PLL_ON;
+  rccosc_cfg.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+  rccosc_cfg.PLL.PLLM = 8;
+  rccosc_cfg.PLL.PLLN = 360;
+  rccosc_cfg.PLL.PLLP = RCC_PLLP_DIV2;
+  rccosc_cfg.PLL.PLLQ = 7;
+  if (HAL_RCC_OscConfig(&rccosc_cfg) != HAL_OK) {
+    error_handler();
   }
-  
-  if(HAL_PWREx_EnableOverDrive() != HAL_OK)
-  {
-    /* Initialization Error */
-    Error_Handler();
+
+  if (HAL_PWREx_EnableOverDrive() != HAL_OK) {
+    error_handler();
   }
-  
-  /* Select PLL as system clock source and configure the HCLK, PCLK1 and PCLK2 
-     clocks dividers */
-  RCC_ClkInitStruct.ClockType = (RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2);
-  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
-  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;  
-  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;  
-  if(HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_5) != HAL_OK)
-  {
-    /* Initialization Error */
-    Error_Handler();
+
+  // configure clock
+  rccclk_cfg.ClockType = (RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_HCLK |
+                          RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2);
+  rccclk_cfg.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  rccclk_cfg.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  rccclk_cfg.APB1CLKDivider = RCC_HCLK_DIV4;
+  rccclk_cfg.APB2CLKDivider = RCC_HCLK_DIV2;
+  if (HAL_RCC_ClockConfig(&rccclk_cfg, FLASH_LATENCY_5) != HAL_OK) {
+    error_handler();
   }
 }
 
-/**
-  * @brief  This function is executed in case of error occurrence.
-  * @param  None
-  * @retval None
-  */
-static void Error_Handler(void)
-{
-  /* Turn LED1 off */
+static void error_handler(void) {
   BSP_LED_Off(LED1);
-  while(1)
-  {
+  BSP_LED_Off(LED2);
+  BSP_LED_Off(LED3);
+  while (1) {
   }
 }
 
-#ifdef  USE_FULL_ASSERT
-/**
-  * @brief  Reports the name of the source file and the source line number
-  *         where the assert_param error has occurred.
-  * @param  file: pointer to the source file name
-  * @param  line: assert_param error line source number
-  * @retval None
-  */
-void assert_failed(uint8_t *file, uint32_t line)
-{
-  /* User can add his own implementation to report the file name and line number,
-     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
-
-  /* Infinite loop */
-  while (1)
-  {
-  }
-}
+#ifdef USE_FULL_ASSERT
+void assert_failed(uint8_t *file, uint32_t line) { error_handler(); }
 #endif
-
-/**
-  * @}
-  */
-
-/**
-  * @}
-  */
